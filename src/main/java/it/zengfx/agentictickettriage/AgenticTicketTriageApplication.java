abbf31b1579b309a667a283322fbb5e3794511f5
Roberto <zenobioroberto@gmail.com>
2026-05-14 07:04:40 +0200

    Initial commit

---
+++ src/main/java/it/zengfx/agentictickettriage/AgenticTicketTriageApplication.java
@@ -0,0 +1,13 @@
+package it.zengfx.agentictickettriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticTicketTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticTicketTriageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/zengfx/agentictickettriage/AgenticTicketTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package it.zengfx.agentictickettriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenticTicketTriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
